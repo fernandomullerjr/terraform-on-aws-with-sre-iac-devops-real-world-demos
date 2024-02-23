bc00c3bae0f624d233d0dba2dd1e24aef285dd6f
Fernando Muller Junior <fernandomj90@gmail.com>
2024-02-22 21:28:33 -0300

    33. Step-01: Implement Variable Lists, Maps and also Meta-Argument Count.

--- Secao5-Terraform-Loops-MetaArguments-SplatOperator-and-Functions/terraform-manifests/c2-variables.tf
+++ Secao5-Terraform-Loops-MetaArguments-SplatOperator-and-Functions/terraform-manifests/c2-variables.tf
@@ -20,3 +20,21 @@ variable "instance_keypair" {
   default     = "terraform-key"
 }
 
+
+# AWS EC2 Instance Type - List
+variable "instance_type_list" {
+  description = "EC2 Instance Type"
+  type = list(string)
+  default = ["t2.micro", "t2.small", "t2.large"]  
+}
+
+# AWS EC2 Instance Type - Map
+variable "instance_type_map" {
+  description = "EC2 Instance Type"
+  type = map(string)
+  default = {
+    "dev" = "t2.micro"
+    "qa" = "t2.small"
+    "prod" = "t2.large"
+  }
+}
