7db3519b28108f922c41da94a36b9a920bdfae55
Fernando Muller Junior <fernandomj90@gmail.com>
2024-02-22 21:52:42 -0300

    33. Step-01: Implement Variable Lists, Maps and also Meta-Argument Count.

--- Secao5-Terraform-Loops-MetaArguments-SplatOperator-and-Functions/terraform-manifests/c5-ec2instance.tf
+++ Secao5-Terraform-Loops-MetaArguments-SplatOperator-and-Functions/terraform-manifests/c5-ec2instance.tf
@@ -1,11 +1,28 @@
 # EC2 Instance
 resource "aws_instance" "myec2vm" {
   ami                    = data.aws_ami.amzlinux2.id
-  instance_type          = var.instance_type
+  #instance_type          = var.instance_type
+
+  
+  # How to reference List values ?
+  #instance_type = var.instance_type_list[1]
+
+  # How to reference Map values ?
+  instance_type = var.instance_type_map["dev"]
+
   user_data              = file("${path.module}/app1-install.sh")
   key_name               = var.instance_keypair
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
-  tags = {
-    "Name" = "EC2 Demo 2"
-  }
+
+  # Meta-Argument Count
+  count = 2
+
+  # count.index
+    tags = {
+      "Name" = "EC2-Count-Demo-${count.index}"
+    }
+
+  # tags = {
+  #   "Name" = "EC2 Demo 2"
+  # }
 }
