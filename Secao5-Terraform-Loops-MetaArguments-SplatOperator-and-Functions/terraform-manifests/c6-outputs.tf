5cd9c6324697af259e083366c1b9089ade878f85
Fernando Muller Junior <fernandomj90@gmail.com>
2024-03-16 18:14:30 -0300

    37. Step-05: Implement Outputs with toset, tomap functions and create and destroy.

--- Secao5-Terraform-Loops-MetaArguments-SplatOperator-and-Functions/terraform-manifests/c6-outputs.tf
+++ Secao5-Terraform-Loops-MetaArguments-SplatOperator-and-Functions/terraform-manifests/c6-outputs.tf
@@ -13,23 +13,23 @@
 # }
 
 
-# Output - For Loop with List
-output "for_output_list" {
-  description = "For Loop with List"
-  value = [for instance in aws_instance.myec2vm: instance.public_dns]
-}
-
-# Output - For Loop with Map
-output "for_output_map1" {
-  description = "For Loop with Map"
-  value = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
-}
-
-# Output - For Loop with Map Advanced
-output "for_output_map2" {
-  description = "For Loop with Map - Advanced"
-  value = {for c, instance in aws_instance.myec2vm: c => instance.public_dns}
-}
+## Output - For Loop with List
+#output "for_output_list" {
+#  description = "For Loop with List"
+#  value = [for instance in aws_instance.myec2vm: instance.public_dns]
+#}
+#
+## Output - For Loop with Map
+#output "for_output_map1" {
+#  description = "For Loop with Map"
+#  value = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
+#}
+#
+## Output - For Loop with Map Advanced
+#output "for_output_map2" {
+#  description = "For Loop with Map - Advanced"
+#  value = {for c, instance in aws_instance.myec2vm: c => instance.public_dns}
+#}
 
 # Output Legacy Splat Operator (Legacy) - Returns the List
 /*
@@ -40,7 +40,37 @@ output "legacy_splat_instance_publicdns" {
 */
 
 # Output Latest Generalized Splat Operator - Returns the List
-output "latest_splat_instance_publicdns" {
-  description = "Generalized latest Splat Operator"
-  value = aws_instance.myec2vm[*].public_dns
+#output "latest_splat_instance_publicdns" {
+#  description = "Generalized latest Splat Operator"
+#  value = aws_instance.myec2vm[*].public_dns
+#}
+
+
+### AULA 37
+# EC2 Instance Public IP with TOSET
+output "instance_publicip" {
+  description = "EC2 Instance Public IP"
+  #value = aws_instance.myec2vm.*.public_ip   # Legacy Splat
+  #value = aws_instance.myec2vm[*].public_ip  # Latest Splat
+  value = toset([
+      for myec2vm in aws_instance.myec2vm : myec2vm.public_ip
+    ])  
+}
+
+# EC2 Instance Public DNS with TOSET
+output "instance_publicdns" {
+  description = "EC2 Instance Public DNS"
+  #value = aws_instance.myec2vm[*].public_dns  # Legacy Splat
+  #value = aws_instance.myec2vm[*].public_dns  # Latest Splat
+  value = toset([
+      for myec2vm in aws_instance.myec2vm : myec2vm.public_dns
+    ])    
+}
+
+# EC2 Instance Public DNS with MAPS
+output "instance_publicdns2" {
+  value = tomap({
+    for s, myec2vm in aws_instance.myec2vm : s => myec2vm.public_dns
+    # S intends to be a subnet ID
+  })
 }
