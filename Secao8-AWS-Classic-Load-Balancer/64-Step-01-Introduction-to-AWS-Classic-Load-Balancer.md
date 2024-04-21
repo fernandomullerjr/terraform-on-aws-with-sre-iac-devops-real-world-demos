a5265a5fae51711d0712f557d0421293681cc436
Fernando Muller Junior <fernandomj90@gmail.com>
2024-04-21 16:26:07 -0300

    65. Step-02: Create Classic Load Balancer Terraform Module Configuration.

--- Secao8-AWS-Classic-Load-Balancer/64-Step-01-Introduction-to-AWS-Classic-Load-Balancer.md
+++ Secao8-AWS-Classic-Load-Balancer/64-Step-01-Introduction-to-AWS-Classic-Load-Balancer.md
@@ -25,3 +25,12 @@ git status
 - Define Outputs for Load Balancer
 - Access and test
 - [Terraform Module AWS ELB](https://registry.terraform.io/modules/terraform-aws-modules/elb/aws/latest) used
+
+
+## Step-02: Copy all templates from previous section 
+- Copy `terraform-manifests` folder from `07-AWS-EC2Instance-and-SecurityGroups`
+- We will add four more files in addition to previous section `07-AWS-EC2Instance-and-SecurityGroups`
+- c5-05-securitygroup-loadbalancersg.tf
+- c10-01-ELB-classic-loadbalancer-variables.tf
+- c10-02-ELB-classic-loadbalancer.tf
+- c10-03-ELB-classic-loadbalancer-outputs.tf
---
+++ Secao8-AWS-Classic-Load-Balancer/manifestos/c1-versions.tf
@@ -0,0 +1,33 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+
+  profile = "fernandomullerjunior-labs/AdministratorAccess"
+
+    assume_role {
+    role_arn     = "arn:aws:iam::058264180843:role/admin-role-account-sandbox"
+    # (Optional) The external ID created in step 1c.
+    external_id = "7755"
+    session_name = "terraform-session"
+  }
+
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
