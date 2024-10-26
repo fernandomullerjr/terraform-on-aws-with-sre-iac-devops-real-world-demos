79907f05a5f45ffef8f5e4401abf8569885761b7
Fernando Müller Junior <fernandomj90@gmail.com>
2024-10-26 15:24:18 -0300

    78. Step-05,06: Create ALB TG1 and TG2, HTTPS Redirect, Fixed Response.

--- Secao10-AWS-ALB-Context-path/manifestos/c11-acm-certificatemanager.tf
+++ Secao10-AWS-ALB-Context-path/manifestos/c11-acm-certificatemanager.tf
@@ -1,11 +1,11 @@
 # ACM Module - To create and Verify SSL Certificates
 module "acm" {
-  source  = "terraform-aws-modules/acm/aws"
+  source = "terraform-aws-modules/acm/aws"
   #version = "2.14.0"
   version = "5.0.0"
 
-  domain_name  = trimsuffix(data.aws_route53_zone.mydomain.name, ".")
-  zone_id      = data.aws_route53_zone.mydomain.zone_id 
+  domain_name = trimsuffix(data.aws_route53_zone.mydomain.name, ".")
+  zone_id     = data.aws_route53_zone.mydomain.zone_id
 
   subject_alternative_names = [
     "*.devopsmind.shop",
@@ -14,8 +14,8 @@ module "acm" {
 
   # Module Upgrade Change-1
   # Validation Method
-  validation_method = "DNS"
-  wait_for_validation = true  
+  validation_method   = "DNS"
+  wait_for_validation = true
 }
 
 # Output ACM Certificate ARN
@@ -23,6 +23,6 @@ output "acm_certificate_arn" {
   description = "The ARN of the certificate"
   # Module Upgrade Change-2  
   #value       = module.acm.this_acm_certificate_arn
-  value       = module.acm.acm_certificate_arn
+  value = module.acm.acm_certificate_arn
 }
 
